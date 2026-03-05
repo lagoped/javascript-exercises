c864945a0205ac39b55c183c06879a1c308d4b71
lagopede <lagopede334@hotmail.fr>
2026-03-05 10:01:50 -0500

    two more exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 0,)
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce(
+    (accumulator, currentValue) => accumulator * currentValue, 1,)
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  // return Math.pow(base, exponent)
+  return base**exponent
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if(a <= 1) {
+    return 1;
+  }
+
+  let sum = 1;
+
+  while (a > 1) {
+    sum = sum * a;
+    a--;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
 
+    sentence = sentence.toLowerCase().replaceAll(" ", "").replaceAll(".", "").replaceAll(",", "").replaceAll("!", "").replaceAll("?", "");
+
+    backwardSentence  = sentence.split("").reverse().join("");
+
+    return backwardSentence === sentence;
 };
 
 // Do not edit below this line
